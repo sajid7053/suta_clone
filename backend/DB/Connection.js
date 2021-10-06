b43a7f3a0f5317dd216d0051824ed51551428828
sajid7053 <39021751+sajid7053@users.noreply.github.com>
2021-10-06 22:47:17 +0530

    Updated db file

--- backend/DB/Connection.js
+++ backend/DB/Connection.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 //Connecting the mongodb cloud application
-const URI = "YOUR MONGO CLOUD URI"
+const URI = "mongodb+srv://sajid_01:sajid_01@cluster0.lsme9.mongodb.net/suta?retryWrites=true&w=majority"
 
 const connectDB = async() => {//awaiting the async functions
     await mongoose.connect(URI, {
